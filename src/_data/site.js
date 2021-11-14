d96b8b1be61dad36bdbc19d62a1b8a57bf972c80
Arpan KC <nipeshkc7@gmail.com>
2021-11-14 18:57:31 +1100

    update site data

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,25 +1,39 @@
 require("dotenv").config();
 
-const ghostContentAPI = require("@tryghost/content-api");
-
-// Init Ghost API
-const api = new ghostContentAPI({
-  url: process.env.GHOST_API_URL,
-  key: process.env.GHOST_CONTENT_API_KEY,
-  version: "v2"
-});
-
 // Get all site information
 module.exports = async function() {
-  const siteData = await api.settings
-    .browse({
-      include: "icon,url"
-    })
-    .catch(err => {
-      console.error(err);
-    });
-
-  if (process.env.SITE_URL) siteData.url = process.env.SITE_URL;
-
-  return siteData;
+  return {
+      title: "Arpan's Blog",
+      description: 'Javascript, Cloud, Rust and much more ...',
+      logo: 'https://arpankc.com/img/download.png',
+      icon: 'https://arpankc.com/img/icon.png',
+      cover_image: 'https://static.ghost.org/v1.0.0/images/blog-cover.jpg',
+      facebook: 'ghost',
+      twitter: '@ghost',
+      navigation: [
+        {
+          label: 'Starter Repo',
+          url: 'https://github.com/TryGhost/eleventy-starter-ghost'
+        },
+        { label: 'Home', url: '/' },
+        { label: 'Tag', url: '/tag/getting-started/' },
+        { label: 'Author', url: '/author/ghost/' },
+        { label: 'Help', url: 'https://docs.ghost.org' }
+      ],
+      secondary_navigation: [],
+      meta_title: null,
+      meta_description: null,
+      og_image: null,
+      og_title: null,
+      og_description: null,
+      twitter_image: null,
+      twitter_title: null,
+      twitter_description: null,
+      members_support_address: 'noreply@eleventy.ghost.io',
+      url: process.env.SITE_URL || 'https://eleventy.ghost.io/',
+      ghost_head: null,
+      ghost_foot: null,
+      active_timezone: 'Etc/UTC',
+      default_locale: 'en'
+    }
 };
